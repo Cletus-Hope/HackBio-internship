2233fbf69ad15de059d1d94f5acd9bebb04c5cc3
Hope Cletus Okon <92362017+Cletus-Hope@users.noreply.github.com>
2026-04-24 14:25:38 +0100

    Add files via upload

---
+++ stage zero/simple_code.R
@@ -0,0 +1,10 @@
+name = "Hope Cletus Okon"
+aff1 = "University of Greenwich, United Kingdom"
+aff2 = "University of Calabar, Nigeria"
+gene = "invA"
+organism = "Salmonella"
+
+print("Hi, my name is " + name +
+      ", a researcher at " + aff1 + " and " + aff2 +
+      ". My favorite gene is " + gene +
+      " in " + organism + ".")
